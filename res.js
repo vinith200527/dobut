1fe037b7f7e526aecc8f5eaac7c094bb3432d533
vinith200527 <157975176+Ashwin2209@users.noreply.github.com>
2024-07-16 09:37:13 +0530

    add

--- res.js
+++ res.js
@@ -1,6 +1,6 @@
 const btn = document.getElementsByClassName("btn")[0];
-const navbar = document.getElementsByClassName("nav-link")[0];
+const navbar = document.getElementsByClassName("nav-list")[0];
 
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', ()=> {
         navbar.classList.toggle('active');
     });
